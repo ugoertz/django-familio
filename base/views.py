df66761500e516968d0a8a5ef09fa8abb5fde75f
Ulrich Goertz <ug@geometry.de>
2015-02-21 00:07:48 +0100

    Add calendar of nearby birthdays/deathdays to home view.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 """ Views for the base application """
 
+import datetime
 from django.shortcuts import render
 # from django.views.generic import View
 # import watson
@@ -11,10 +12,25 @@ def home(request):
     """ Default view for the root """
 
     # pylint: disable=no-member
-    return render(request, 'base/home.html',
-                  {'personen': Person.objects.all().order_by('-date_added')[:5],
-                   'notes': Note.objects.filter(published=True)
-                                .order_by('date_added')[:5], })
+    dates = [datetime.date.today() + (i-4) * datetime.timedelta(days=1)
+             for i in range(20)]
+    birthdeathdays = [
+            (d,
+             Person.objects
+             .filter(datebirth__endswith=d.strftime('-%m-%d')),
+             Person.objects
+             .filter(datedeath__endswith=d.strftime('-%m-%d')), )
+            for d in dates]
+    birthdeathdays = [(d, born, died)
+                      for d, born, died in birthdeathdays if born or died]
+
+    # pylint: disable=no-member
+    return render(
+            request, 'base/home.html',
+            {'personen': Person.objects.all().order_by('-date_added')[:5],
+             'birthdeathdays': birthdeathdays, 
+             'notes': Note.objects.filter(published=True)
+                          .order_by('date_added')[:5], })
 
 
 # class SearchView(View):
