a9254dedb538c0c76711b12a880c98bae060bfd4
Ulrich Goertz <ug@geometry.de>
2015-02-25 22:08:53 +0100

    Improve handling of RST error messages in views.

--- notaro/static/js/rsterrors.js
+++ notaro/static/js/rsterrors.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    if ($(".system-message").size()) {
+    if ($(".system-messages, .system-message").size()) {
         $("#errormsg").html("Fehler anzeigen");
         $("#errormsg").css("display", "");
         $("#errormsg").click(function() {
             if ($("#errormsg").html() == "Fehler anzeigen") {
-                $(".system-message").css("display", "block");
+                $(".system-messages, .system-message").css("display", "block");
                 $("#errormsg").html("Fehler ausblenden");
             } else {
-                $(".system-message").css("display", "none");
+                $(".system-messages, .system-message").css("display", "none");
                 $("#errormsg").html("Fehler anzeigen");
             }
         });
