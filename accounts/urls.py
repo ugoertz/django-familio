c5db2d928142327dfe594eb6cd13d0fe4ee9f8bb
Ulrich Goertz <ug@geometry.de>
2015-02-28 22:26:19 +0100

    Fix accounts/urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from .views import InviteView, AcceptInvitationView
 urlpatterns = patterns('',
                        url(r'^invite/', InviteView.as_view(),
                            name="send-invitation"),
-                       url(r'^register/(?P<key>[a-f0-9]+)/$',
+                       url(r'^register/(?P<key>\w+)/$',
                            AcceptInvitationView.as_view(),
                            name="accept-invitation"),
                        url(r'^(?P<username>[\.\w-]+)/edit/$',
