757bd4d6987dac6de252ee85115d0be876071be7
Ulrich Goertz <ug@geometry.de>
2015-04-04 14:19:47 +0200

    Update settings file for testing.

--- familio/settings/test.py
+++ familio/settings/test.py
@@ -4,18 +4,70 @@ Use this settings file when running tests.
 These settings overrides what's in settings/base.py
 """
 
+from __future__ import absolute_import
+import json
+import os
+
 from .base import *
 
+# PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__) + "../../../")
+from .base import *
+
+secrets = json.load(file(os.path.join(PROJECT_ROOT, '../secrets_test.json')))
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'djfdbtest',
+        'USER': 'djftest',
+        'PASSWORD': secrets['DB_PASSWORD'],
+        'HOST': '',
+        'PORT': '',
+        # 'OPTIONS': {
+        #     'init_command': 'SET storage_engine=InnoDB',
+        #     'charset' : 'utf8',
+        #     'use_unicode' : True,
+        # },
+        # 'TEST_CHARSET': 'utf8',
+        # 'TEST_COLLATION': 'utf8_general_ci',
     },
+    # 'slave': {
+    #     ...
+    # },
 }
 
-SECRET_KEY = '4400v=mppsfjzkj7+2$v9=439zlvnu!w5chi4f!i2v=&4ynlk4'
+# Recipients of traceback emails and other notifications.
+ADMINS = (
+    ('admin', 'root@localhost'),
+)
+MANAGERS = ADMINS
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# Is this a development instance? Set this to True on development/master
+# instances and False on stage/prod.
+DEV = True
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['localhost', ]
+
+# Log settings
+# LOGGING_CONFIG = None
+
+ADMIN_USERNAME = 'admin'
+SITE_ID = 1
+
+SECRET_KEY = secrets['SECRET_KEY_%d' % SITE_ID]
+
+FILEBROWSER_VERSIONS_BASEDIR = '%d_versions/' % SITE_ID
+FILEBROWSER_DIRECTORY = '%d_uploads/' % SITE_ID
+STATIC_URL = '/static/'
+
+DOCUMENTATION_URL =\
+        os.path.join(PROJECT_ROOT, '/docs/_build/html/')
+ALLOWABLE_VALUES = ('DOCUMENTATION_URL', )
+
