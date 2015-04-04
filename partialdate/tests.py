3971d370a8adcd20a4564fc48076e8aeecedaeb2
Ulrich Goertz <ug@geometry.de>
2015-04-04 14:21:55 +0200

    Add first tests for PartialDate class.

--- partialdate/tests.py
+++ partialdate/tests.py
@@ -1,3 +1,26 @@
+# -*- coding: utf8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 from django.test import TestCase
 
-# Create your tests here.
+from .fields import PartialDate
+
+class PartialDateTests(TestCase):
+
+    def test_create_partialdate(self):
+        pd1 = PartialDate(2000, 1, 1)
+        self.assertEqual(pd1.year, 2000)
+        self.assertEqual(pd1.month, 1)
+        self.assertEqual(pd1.day, 1)
+        self.assertEqual(pd1.isoformat(), '2000-01-01')
+        self.assertEqual(unicode(pd1), '2000-01-01')
+        self.assertEqual(pd1.weekday, 5)
+
+        pd2 = PartialDate(2001, 1)
+        self.assertEqual(pd2.year, 2001)
+        self.assertEqual(pd2.month, 1)
+        self.assertEqual(pd2.day, None)
+        self.assertEqual(pd2.isoformat(), '2001-01')
+
