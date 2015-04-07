85d99f9e4c8571cb522cebc1cec7683a5df48eea
Ulrich Goertz <ug@geometry.de>
2015-04-07 13:59:03 +0200

    Change title of LaTeX documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -178,7 +178,7 @@ latex_font_size = '11pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'project.tex', u'project Documentation',
+  ('index', 'project.tex', u'unserefamilie.net Dokumentation',
    u'Ulrich Görtz', 'manual'),
 ]
 
