860071a4341ca4791b012c5c2020751da719ad51
Ulrich Goertz <ug@geometry.de>
2015-04-14 14:11:27 +0200

    Send userena messages as pybb topic notifications.

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,7 +1,18 @@
-from django.core.mail import send_mail
-from django.dispatch import receiver
-from django.contrib.sites.models import Site
+# -*- coding: utf8 -*-
 
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from pybb.models import Post
+from userena.contrib.umessages.models import Message
 from userena.contrib.umessages.signals import email_sent
 
 
@@ -15,4 +26,37 @@ def send_email_notification(sender, **kwargs):
               [user.email for user in msg.recipients.all()
                   if user.userprofile.email_on_message],
               fail_silently=True)
+
+
+@receiver(post_save, sender=Post)
+def notify_topic_subscribers(instance, **kwargs):
+    post = instance
+    topic = post.topic
+    if post != topic.head:
+        User = get_user_model()
+        admin = User.objects.get(username=settings.ADMIN_USERNAME)
+        for user in topic.subscribers.exclude(pk=post.user.pk):
+            # get site of the poster
+            site = Site.objects.get_current()
+
+            # Is user active at this site? If not, replace by one of the sites
+            # of the user
+            if not site in user.userprofile.sites.all():
+                site = user.userprofile.sites.all()[0]
+
+            # could improve this by using a template instead of a hard-coded
+            # message text
+            message =\
+                'Hallo %s, im Forum ist im Thema %s ' % (user.first_name,
+                                                        topic.name) +\
+                'von %s ' % post.user.get_full_name() +\
+                'ein neuer Beitrag veröffentlicht worden.\n\n' +\
+                'Anschauen: http://%s%s\n\nAbo löschen: http://%s%s' % (
+                   site.domain,
+                   reverse('pybb:topic', kwargs={'pk': topic.pk}) +\
+                           '?first-unread=1',
+                   site.domain,
+                   reverse('pybb:delete_subscription', args=[post.topic.id]))
+            Message.objects.send_message(admin, [user, ], message)
+
 
