2228fc7c88b13083be1f7b54175ea3ed62371c8a
Ulrich Goertz <ug@geometry.de>
2015-04-23 20:02:14 +0200

    Add include_if_exists template tag; use for web analytics, impressum, custom block on home page.

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.utils.functional import allow_lazy
 from django.utils.encoding import force_unicode
 from django.template import Node
+from django.template.defaulttags import CommentNode
+from django.template.loader_tags import do_include
 
 
 register = template.Library()
@@ -57,3 +59,28 @@ def settings_value(name):
     if name in getattr(settings, 'ALLOWABLE_VALUES', []):
         return getattr(settings, name, '')
     return ''
+
+
+@register.tag('include_if_exists')
+def do_include_maybe(parser, token):
+    "Source: http://stackoverflow.com/a/18951166/15690"
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise template.TemplateSyntaxError(
+            "%r tag takes at least one argument: "
+            "the name of the template to be included." % bits[0])
+
+    try:
+        silent_node = do_include(parser, token)
+    except template.TemplateDoesNotExist:
+        # Django < 1.7
+        return CommentNode()
+
+    _orig_render = silent_node.render
+    def wrapped_render(*args, **kwargs):
+        try:
+            return _orig_render(*args, **kwargs)
+        except template.TemplateDoesNotExist:
+            return CommentNode()
+    silent_node.render = wrapped_render
+    return silent_node
