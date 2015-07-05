f465062acad693d95f386b2025fdd5a06e6905db
Ulrich Goertz <ug@geometry.de>
2015-07-05 22:34:30 +0200

    Fix small bug in database settings.

--- familio/settings/base.py
+++ familio/settings/base.py
@@ -26,7 +26,7 @@ DATABASES = {
         'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': 'db',
+        'HOST': os.environ['POSTGRES_HOST'],
         'PORT': '',
     },
 }
