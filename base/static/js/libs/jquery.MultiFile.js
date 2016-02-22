9a6acf075097b0c2a408a18de26c3d32f6df5e92
Ulrich Goertz <ug@geometry.de>
2016-02-22 22:17:37 +0100

    Fix encoding pbm in jquery.Multifile.

--- base/static/js/libs/jquery.MultiFile.js
+++ base/static/js/libs/jquery.MultiFile.js
@@ -380,7 +380,7 @@ if (window.jQuery)(function ($) {
 							// Handle error
 							MultiFile.error(ERROR.join('\n\n'));
 
-							// 2007-06-24: BUG FIX - Thanks to Adrian Wróbel <adrian [dot] wrobel [at] gmail.com>
+							// 2007-06-24: BUG FIX - Thanks to Adrian WrÃ³bel <adrian [dot] wrobel [at] gmail.com>
 							// Ditch the trouble maker and add a fresh new element
 							MultiFile.n--;
 							MultiFile.addSlave(newEle[0], slave_count);
