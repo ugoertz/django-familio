320f81670ab0c46407c172b4802a463c92270230
Ulrich Goertz <ug@geometry.de>
2019-11-02 21:44:44 +0100

    Fix include_if_exists template tag

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -71,18 +71,13 @@ def do_include_maybe(parser, token):
             "%r tag takes at least one argument: "
             "the name of the template to be included." % bits[0])
 
-    try:
-        silent_node = do_include(parser, token)
-    except template.TemplateDoesNotExist:
-        # Django < 1.7
-        return CommentNode()
-
+    silent_node = do_include(parser, token)
     _orig_render = silent_node.render
 
     def wrapped_render(*args, **kwargs):
         try:
             return _orig_render(*args, **kwargs)
         except template.TemplateDoesNotExist:
-            return CommentNode()
+            return ''
     silent_node.render = wrapped_render
     return silent_node
